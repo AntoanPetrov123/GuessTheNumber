40eb80e158c8b85b1b35c6011f23c0c58800d2fa
Antoan Petrov <72857425+AntoanPetrov123@users.noreply.github.com>
2022-08-17 15:59:31 +0300

    Init commit

---
+++ guessANumber.js
@@ -0,0 +1,113 @@
+// let colors = require('colors');
+
+// colors.enable()
+
+// const readline = require('readline').createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// let basicNumber = 100;
+
+// function guessTheNumber(maxNumber) {
+//   let addPoints = 100;
+//   let computerGuess = Number(Math.floor(Math.random() * maxNumber));
+
+//   let recursiveAcyncreadLine = () => {
+//     readline.question(`Guess the number (0 - ${maxNumber}): `, number => {
+
+//       let guess = Number(number);
+
+//       if (guess >= 0 && guess <= maxNumber) {
+//         if (guess === computerGuess) {
+//           console.log('You guess it!'.bold.yellow);
+
+//           readline.question('Start next level y/n: ', answer => {
+//             if (answer === 'y') {
+//               maxNumber += addPoints;
+//               guessTheNumber(maxNumber);
+//             } else {
+//               return readline.close();
+//             }
+//           });
+//         } else if (guess < computerGuess) {
+//           console.log('Too Low!'.bold.green);
+//           recursiveAcyncreadLine();
+//         } else if (guess > computerGuess) {
+//           console.log('Too High!'.bold.red);
+//           recursiveAcyncreadLine();
+//         }
+//       } else {
+//         console.log('Invalid input! Try again...'.bold.underline.red);
+//         recursiveAcyncreadLine();
+//       }
+//     });
+//   }
+
+//   recursiveAcyncreadLine();
+// }
+// guessTheNumber(basicNumber);
+let colors = require('colors');
+
+colors.enable()
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let basicNumber = 100;
+let basicGuessTries = 14;
+
+function guessTheNumber(maxNumber, guessTries) {
+    let addPoints = 100;
+    let computerGuess = Number(Math.floor(Math.random() * maxNumber));
+
+    let recursiveAcyncreadLine = (guessTries) => {
+        readline.question(`You have ${guessTries + 1} moves left \nGuess the number (0 - ${maxNumber}): `, number => {
+
+            let guess = Number(number);
+
+            if (guessTries > 0) {
+
+                if (guess >= 0 && guess <= maxNumber) {
+                    if (guess === computerGuess) {
+                        console.log('You guess it!'.bold.yellow);
+
+                        readline.question('Start next level y/n: ', answer => {
+                            if (answer === 'y') {
+                                maxNumber += addPoints;
+                                basicGuessTries -= 2;
+                                guessTheNumber(maxNumber, basicGuessTries);
+                            } else {
+                                return readline.close();
+                            }
+                        });
+                    } else if (guess < computerGuess) {
+                        console.log('Too Low!'.bold.green);
+                        recursiveAcyncreadLine(--guessTries);
+                    } else if (guess > computerGuess) {
+                        console.log('Too High!'.bold.red);
+                        recursiveAcyncreadLine(--guessTries);
+                    }
+                } else {
+                    console.log('Invalid input! Try again...'.bold.underline.red);
+                    recursiveAcyncreadLine(guessTries);
+                }
+            } else {
+                console.log('Oops, you are out of moves...'.bold.underline.red);
+                readline.question('Start again y/n: '.bold.underline.green, answer => {
+                    if (answer === 'y') {
+                        maxNumber = basicNumber;
+                        guessTheNumber(maxNumber, basicGuessTries);
+                    } else {
+                        return readline.close();
+                    }
+                });
+            }
+        });
+    }
+
+    recursiveAcyncreadLine(guessTries);
+}
+guessTheNumber(basicNumber, basicGuessTries);
